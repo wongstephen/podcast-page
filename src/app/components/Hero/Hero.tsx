71385e5cc06167494531b950e7f6198f39cd6a82
Stephen Wong <wongstephenky@gmail.com>
2025-07-31 09:59:45 -0700

    feature: subscribe animation

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -7,6 +7,7 @@ import { DeezerIcon } from '@/lib/components/atoms/Icons/DeezerIcon';
 import { AppleIcon } from '@/lib/components/atoms/Icons/AppleIcon';
 
 import styles from './Hero.module.css';
+import Link from 'next/link';
 
 export default function Hero() {
   const dict = getDictionary('en');
@@ -31,24 +32,24 @@ export default function Hero() {
           <p className={styles.subscribeTitle}>{dict.home.hero.subscribe}</p>
           {/* TODO: If this is repeated in other parts of the site, consider creating a shared component for the podcast subscription links. */}
           <div className={styles.subscribeLinksContainer}>
-            <div className={styles.subscribeLink}>
+            <Link className={styles.subscribeLink} href="#">
               <SpotifyIcon className={styles.podcastIcon} />
               <p className={styles.podcastText}>
                 {dict.home.hero.podcastList.spotify}
               </p>
-            </div>
-            <div className={styles.subscribeLink}>
+            </Link>
+            <Link className={styles.subscribeLink} href="#">
               <DeezerIcon className={styles.podcastIcon} />
               <p className={styles.podcastText}>
                 {dict.home.hero.podcastList.deezer}
               </p>
-            </div>
-            <div className={styles.subscribeLink}>
+            </Link>
+            <Link className={styles.subscribeLink} href="#">
               <AppleIcon className={styles.podcastIcon} />
               <p className={styles.podcastText}>
                 {dict.home.hero.podcastList.apple}
               </p>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
