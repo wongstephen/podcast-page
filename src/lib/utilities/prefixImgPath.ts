7b0f2fd61828089f25f71a3ef0d8aee3018e4334
Stephen Wong <wongstephenky@gmail.com>
2025-08-01 21:56:18 -0700

    chore: update env

--- src/lib/utilities/prefixImgPath.ts
+++ src/lib/utilities/prefixImgPath.ts
@@ -1,7 +1,4 @@
-const basePath =
-  process.env.NEXT_PUBLIC_ENV === 'not-github'
-    ? ''
-    : process.env.NEXT_PUBLIC_PAGES_BASE_PATH || '';
+const basePath = process.env.NEXT_PUBLIC_PAGES_BASE_PATH || '';
 
 export function prefixImgPath(path: string): string {
   return `${basePath}${path}`;
