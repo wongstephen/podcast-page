bdab7ed2014c025a62fa75add0aabc0aa8141b1d
Stephen Wong <wongstephenky@gmail.com>
2025-08-01 21:09:22 -0700

    chore: update env

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // uncomment when deploying to github pages
-  // basePath: '/podcast-page'
+  // basePath: '/podcast-page',
 };
 
 export default nextConfig;
