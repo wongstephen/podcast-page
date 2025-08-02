77af85da1fd7e3b4525114b7bb9f352689802a00
Stephen Wong <wongstephenky@gmail.com>
2025-08-01 19:29:26 -0700

    chore: update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Quicksand } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -6,13 +7,23 @@ export const metadata: Metadata = {
   description: 'The Aircall Podcast - The power of conversation.',
 };
 
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-quicksand',
+  display: 'swap',
+  style: 'normal',
+  fallback: ['sans-serif'],
+  preload: true,
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${quicksand.variable}`}>
       <body>{children}</body>
     </html>
   );
