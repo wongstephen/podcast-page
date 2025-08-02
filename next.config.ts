7a552b53d26544de9324342f0145f20d1ae5687d
Stephen Wong <wongstephenky@gmail.com>
2025-08-01 19:29:26 -0700

    chore: update font

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // uncomment when deploying to github pages
+  // basePath: '/podcast-page'
+};
 
 export default nextConfig;
